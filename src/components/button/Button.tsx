b81be40429b3caae786204f886491a654220b5f6
Seiryo Pramanda <seiryobayu@gmail.com>
2026-02-17 02:01:06 +0700

    feat: Implement Button component with variant styles, global CSS setup, and Storybook stories.

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,19 +2,28 @@ import "../../app/globals.css";
 
 interface ButtonProps {
   label: string;
-  variant: "solid" | "outline";
+  variant: "solid" | "outline" | "text";
 }
 
-export const Button = ({ label, variant }: ButtonProps) => {
+const Button = ({ label, variant }: ButtonProps) => {
+  const variantStyles = () => {
+    switch (variant) {
+      case "solid":
+        return "bg-primary text-white cursor-pointer hover:bg-primary-hover transition-colors duration-300";
+      case "outline":
+        return "bg-transparent text-primary border border-primary cursor-pointer hover:border-primary-hover hover:text-primary-hover transition-colors duration-300";
+      case "text":
+        return "bg-transparent text-primary cursor-pointer hover:text-primary-hover transition-colors duration-300";
+    }
+  };
+
   return (
     <button
-      className={`px-[30px] py-[9px] rounded-xl text-2xl ${
-        variant === "solid"
-          ? "bg-primary text-white"
-          : "bg-transparent border border-primary text-primary"
-      }`}
+      className={`px-[30px] py-[9px] rounded-xl text-2xl ${variantStyles()}`}
     >
       {label}
     </button>
   );
 };
+
+export default Button;
